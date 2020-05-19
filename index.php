5bc59d2669da151f6d8782221940c72b30b24b99
Filitov <filitov@gmail.com>
2020-05-19 22:18:39 +0800

    new homework

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="zh_Hant">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content= "width=device-width, initial-scale=1.0">
+    <title>作業4</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <h1>Micro-Micro BLOG</h1>
+
+
+    <div id="login-info">
+        <a href="">登入</a>
+    </div>
+
+
+    <div class="note-count">共有 ０ 頁，０ 則文章</div>
+
+
+    <div class="page-nav">
+        <a href="?p=1" class="nav-page-idx nav-page-first">1</a>
+        <span class="nav-page-idx">..</span>
+        <a href="?p=3" class="nav-page-idx">3</a>
+        <a href="?p=4" class="nav-page-idx nav-page-current">4</a>
+        <a href="?p=5" class="nav-page-idx">5</a>
+        <span class="nav-page-idx">..</span>
+        <a href="?p=1" class="nav-page-idx nav-page-last">1</a>
+    </div>
+
+
+    <div id="notes">
+        <div class="note">
+            <div class="title">文章標題</div>
+            <div class="message">文章內容</div>
+        </div>
+        <div class="note">
+            <div class="title">文章標題</div>
+            <div class="message">文章內容</div>
+        </div>
+        <div class="note">
+            <div class="title">文章標題</div>
+            <div class="message">文章內容</div>
+        </div>
+    </div>
+
+
+    <div class="page-nav">
+        <a href="?p=1" class="nav-page-idx nav-page-first">1</a>
+        <span class="nav-page-idx">..</span>
+        <a href="?p=3" class="nav-page-idx">3</a>
+        <a href="?p=4" class="nav-page-idx nav-page-current">4</a>
+        <a href="?p=5" class="nav-page-idx">5</a>
+        <span class="nav-page-idx">..</span>
+        <a href="?p=1" class="nav-page-idx nav-page-last">1</a>
+    </div>
+
+</body>
+</html>
